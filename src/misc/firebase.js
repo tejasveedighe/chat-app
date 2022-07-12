f46035decdf3ed39228efaa865074e6ebcf98013
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-07-12 12:19:11 +0530

    config firebase for app

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQyvh3oYs7aM-CB6WzRQvUsg-N2ucPOic',
+  authDomain: 'chat-app-658a6.firebaseapp.com',
+  projectId: 'chat-app-658a6',
+  storageBucket: 'chat-app-658a6.appspot.com',
+  messagingSenderId: '85461362799',
+  appId: '1:85461362799:web:5d99d552190978afcc459a',
+  measurementId: 'G-DS8YP1TMGP',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
