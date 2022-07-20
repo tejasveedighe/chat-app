77a9a91ac4ba06e8a727eaf833534d93f9f1127a
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-07-20 21:08:37 +0530

    added database link to firebase config and made sigin page using firebase auth

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,8 +1,12 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDQyvh3oYs7aM-CB6WzRQvUsg-N2ucPOic',
   authDomain: 'chat-app-658a6.firebaseapp.com',
+  databaseURL:
+    'https://chat-app-658a6-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'chat-app-658a6',
   storageBucket: 'chat-app-658a6.appspot.com',
   messagingSenderId: '85461362799',
@@ -11,3 +15,5 @@ const firebaseConfig = {
 };
 
 const app = firebase.initializeApp(firebaseConfig);
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import { Icon } from '@rsuite/icons';
+import { Col, Container, Grid, Panel, Row, Button } from 'rsuite';
+import { auth, database } from '../misc/firebase';
 
 function SignIn() {
-  return <div>SignIn</div>;
+  const sigInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}/`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+    } catch (err) {
+      console.log('error: ', err.message);
+    }
+  };
+  // const onFacebookSignIn = () => {
+  //   sigInWithProvider(new firebase.auth.FacebookAuthProvider());
+  // };
+  const onGoogleSignIn = () => {
+    sigInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>Progressive Chat App</p>
+              </div>
+              <div className="mt-3">
+                <Button
+                  block
+                  color="green"
+                  appearance="primary"
+                  onClick={onGoogleSignIn}
+                >
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 }
 
 export default SignIn;
